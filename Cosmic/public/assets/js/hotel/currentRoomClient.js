20a5cc0a84125ed6258448f3c7ca149f91eb3437
Mikko Kokkoniemi <mikkokokkoniemi@gmail.com>
2020-04-10 00:07:49 +0300

    fix

--- Cosmic/public/assets/js/hotel/currentRoomClient.js
+++ Cosmic/public/assets/js/hotel/currentRoomClient.js
@@ -1,12 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var currentRoomId = null;
-    // unelegant solution to wait approximately enought that user has logged in
+    var _currentRoomId = null;
+    // unelegant solution to wait approximately enough that user has logged in
     setTimeout(function () {
         setInterval(function () {
             $.getJSON("/api/currentroom", function (data) {
-                if (!currentRoomId || currentRoomId !== data.roomId) {
-                    currentRoomId = data.roomId;
+                if (!_currentRoomId || _currentRoomId !== data.roomId) {
+                    _currentRoomId = data.roomId;
                     var _url = Object.keys(SwaegPlayerUrls).indexOf(data.roomId+'') !== -1 ? SwaegPlayerUrls[data.roomId] : SwaegPlayerUrls['default'];
                     $('#tubeplayer').find('iframe').attr('src', _url);
                 }
